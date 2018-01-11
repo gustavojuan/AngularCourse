c5f44f453ce12945a2a2be3de8e636959fbd61f8
Gus <gustavojuan@gmail.com>
2018-01-11 08:27:04 +0100

    interfaces

---
+++ typescript_1/interfaces/app.js
@@ -0,0 +1,9 @@
+"use strict";
+function enviarMision(xmen) {
+    console.log("enviando a: " + xmen.nombre);
+}
+let wolverine = {
+    nombre: "Wolverine",
+    poder: "Regeneración"
+};
+enviarMision(wolverine);
